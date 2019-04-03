3418ccc684b4c6404bc55c935e626798fea865a5
Henry Beberman <hebeberm@microsoft.com>
2019-04-03 10:42:39 -0700

    Break provisioning into functions

--- Silicon/NXP/iMXPlatformPkg/Drivers/Provisioning/Provisioning.c
+++ Silicon/NXP/iMXPlatformPkg/Drivers/Provisioning/Provisioning.c
@@ -20,39 +20,82 @@
 // This depends on the fTPM implementation. The full response should be sent out
 //  and the host side should be responsible for parsing and truncating it.
 #define EKCERT_SZ 326
+#define TPMIN_SZ 14
+#define TPMOUT_SZ 1000
 
-STATIC CONST CHAR16           mDeviceCertVariableName[] = L"ManufacturerDeviceCert0";
+STATIC CONST CHAR16 mDeviceCertVariableName[] = L"ManufacturerDeviceCert";
 
 EFI_STATUS
 EFIAPI
-ProvisioningInitialize (
-  IN EFI_HANDLE       ImageHandle,
-  IN EFI_SYSTEM_TABLE *SystemTable
-  )
+RecieveBuffer(
+  CHAR8 *requeststring,
+  UINT8 **buffer,
+  UINT32 *length
+)
 {
-  EFI_STATUS Status = EFI_SUCCESS;
+  EFI_STATUS Status;
+  UINT32 i;
+//  UINT32 sum;
+//  UINT32 buflen;
+  UINT8* tempptr;
+  UINTN bytesread;
+
+  Status = EFI_SUCCESS;
+
+  DEBUG((DEBUG_ERROR, requeststring));
+
+  while(SerialPortPoll() == FALSE) {}
+
+  tempptr = (UINT8*) length;
+  for(i = 4, bytesread = 0; i > 0;) {
+    bytesread = SerialPortRead(tempptr, i);
+    i -= bytesread;
+    tempptr += bytesread;
+  }
+
+  DEBUG((DEBUG_WARN, "bytesread: %d\n", bytesread));
+  DEBUG((DEBUG_WARN, "length: 0x%x\n", *length));
+
+  *buffer = AllocateZeroPool (*length);
+  if (*buffer == NULL) {
+    *buffer = NULL;
+    return EFI_OUT_OF_RESOURCES;
+  }
+
+  for(i = *length, tempptr = *buffer; i > 0;) {
+    bytesread = SerialPortRead(tempptr, i);
+    i -= bytesread;
+    tempptr += bytesread;
+  }
+
+  return Status;
+}
+
+EFI_STATUS
+EFIAPI
+TransmitEKCertificate()
+{
+  EFI_STATUS Status;
   EFI_TCG2_PROTOCOL *Tcg2Protocol;
-  UINT8 tpm_in[0xe] = { 0x80, 0x01, // TPM_ST_NO_SESSIONS
+
+  UINT8 tpm_in[TPMIN_SZ] = { 0x80, 0x01, // TPM_ST_NO_SESSIONS
                         0x00, 0x00, 0x00, 0x0e, // Command size is 0xe
                         0x00, 0x00, 0x01, 0x73, // TPM_CC_ReadPublic 0x173
                         0x81, 0x01, 0x00, 0x01}; // 0x81010001 is EK Cert
-  UINT8 tpm_out[1000] = {0};
+  UINT8 tpm_out[TPMOUT_SZ] = {0};
   TPM2_RESPONSE_HEADER *resp_header = (TPM2_RESPONSE_HEADER*)tpm_out;
-
-  UINT32 certlen;
-  UINT8* cert;
-  UINTN bytesread = 0;
 
   Status = gBS->LocateProtocol (&gEfiTcg2ProtocolGuid, NULL, (VOID **) &Tcg2Protocol);
   if (EFI_ERROR(Status)){
     DEBUG((DEBUG_ERROR, "%a: failed to locate protocol. Status: 0x%x\n", __FUNCTION__, Status));
     return Status;
   }
+
   Status = Tcg2Protocol->SubmitCommand (
                           Tcg2Protocol,
-                          0xe,
+                          TPMIN_SZ,
                           tpm_in,
-                          1000,
+                          TPMOUT_SZ,
                           tpm_out
                           );
 
@@ -66,74 +109,151 @@ ProvisioningInitialize (
   for(int x = sizeof(TPM2_RESPONSE_HEADER); x < SwapBytes32(resp_header->paramSize) && x < EKCERT_SZ; x++) {
     DEBUG((DEBUG_ERROR, "%02x\n", tpm_out[x]));
   }
+
   DEBUG((DEBUG_ERROR, "MFG:ekcertend\n"));
+  return EFI_SUCCESS;
+}
+
+EFI_STATUS
+EFIAPI
+RecieveCrossSignedCert()
+{
+  EFI_STATUS Status;
+  UINT32 i;
+  UINT32 sum;
+  UINT32 certlen;
+  UINT8* certptr;
+  UINTN bytesread;
 
   DEBUG((DEBUG_ERROR, "MFG:devicecert\n"));
+
   while(SerialPortPoll() == FALSE) {}
-  int i = 4;
+
   UINT8* bufptr = (UINT8*) &certlen;
-  while(i > 0) {
+  for(i = 4, bytesread = 0; i > 0;) {
     bytesread = SerialPortRead(bufptr, i);
     i -= bytesread;
     bufptr += bytesread;
   }
 
-  DEBUG((DEBUG_ERROR, "bytesread: %d\n", bytesread));
-  DEBUG((DEBUG_ERROR, "certlen: 0x%x\n", certlen));
+  DEBUG((DEBUG_WARN, "bytesread: %d\n", bytesread));
+  DEBUG((DEBUG_WARN, "certlen: 0x%x\n", certlen));
 
-  cert = AllocateZeroPool (certlen);
-  if (cert == NULL) {
+  certptr = AllocateZeroPool (certlen);
+  if (certptr == NULL) {
     return EFI_OUT_OF_RESOURCES;
   }
 
-
-  i = certlen;
-  bufptr = cert;
-  while(i > 0) {
+  for(i = certlen, bufptr = certptr; i > 0;) {
     bytesread = SerialPortRead(bufptr, i);
     i -= bytesread;
     bufptr += bytesread;
   }
 
-  i = certlen;
-  int sum = 0;
-  for(i = 0; i < certlen; i++) {
-    sum += cert[i];
+
+  for(i = 0, sum = 0; i < certlen; i++) {
+    sum += certptr[i];
   }
-  DEBUG((DEBUG_ERROR, "device side checksum: %d\n", sum));
 
-  DEBUG((DEBUG_ERROR, "cert[0]: 0x%02x\n", cert[0]));
-  DEBUG((DEBUG_ERROR, "cert[1]: 0x%02x\n", cert[1]));
-  DEBUG((DEBUG_ERROR, "cert[2]: 0x%02x\n", cert[2]));
-  DEBUG((DEBUG_ERROR, "cert[3]: 0x%02x\n", cert[3]));
-  DEBUG((DEBUG_ERROR, "cert[4]: 0x%02x\n", cert[4]));
-  DEBUG((DEBUG_ERROR, "cert[5]: 0x%02x\n", cert[5]));
-  DEBUG((DEBUG_ERROR, "cert[6]: 0x%02x\n", cert[6]));
-  DEBUG((DEBUG_ERROR, "cert[7]: 0x%02x\n", cert[7]));
-  DEBUG((DEBUG_ERROR, "cert[8]: 0x%02x\n", cert[8]));
-  DEBUG((DEBUG_ERROR, "cert[9]: 0x%02x\n", cert[9]));
-  DEBUG((DEBUG_ERROR, "cert[10]: 0x%02x\n", cert[10]));
-  DEBUG((DEBUG_ERROR, "cert[11]: 0x%02x\n", cert[11]));
-  DEBUG((DEBUG_ERROR, "cert[12]: 0x%02x\n", cert[12]));
-  DEBUG((DEBUG_ERROR, "cert[13]: 0x%02x\n", cert[13]));
-  DEBUG((DEBUG_ERROR, "cert[14]: 0x%02x\n", cert[14]));
+  DEBUG((DEBUG_WARN, "Device side checksum: %d\n", sum));
 
-    Status = gRT->SetVariable (
-                    (CHAR16 *)mDeviceCertVariableName,
-                    &gEfiCallerIdGuid,
-                    EFI_VARIABLE_NON_VOLATILE | EFI_VARIABLE_BOOTSERVICE_ACCESS | EFI_VARIABLE_RUNTIME_ACCESS,
-                    certlen,
-                    (VOID *)cert
-                    );
-    if (EFI_ERROR (Status)) {
-      DEBUG ((
-        EFI_D_ERROR,
-        "Provisioning: Failed to save %s variable to non-volatile storage, Status = %r\n",
-        mDeviceCertVariableName,
-        Status
-        ));
-    }
+/*
+  DEBUG((DEBUG_WARN, "cert[0]: 0x%02x\n", cert[0]));
+  DEBUG((DEBUG_WARN, "cert[1]: 0x%02x\n", cert[1]));
+  DEBUG((DEBUG_WARN, "cert[2]: 0x%02x\n", cert[2]));
+  DEBUG((DEBUG_WARN, "cert[3]: 0x%02x\n", cert[3]));
+  DEBUG((DEBUG_WARN, "cert[4]: 0x%02x\n", cert[4]));
+  DEBUG((DEBUG_WARN, "cert[5]: 0x%02x\n", cert[5]));
+  DEBUG((DEBUG_WARN, "cert[6]: 0x%02x\n", cert[6]));
+  DEBUG((DEBUG_WARN, "cert[7]: 0x%02x\n", cert[7]));
+  DEBUG((DEBUG_WARN, "cert[8]: 0x%02x\n", cert[8]));
+  DEBUG((DEBUG_WARN, "cert[9]: 0x%02x\n", cert[9]));
+  DEBUG((DEBUG_WARN, "cert[10]: 0x%02x\n", cert[10]));
+  DEBUG((DEBUG_WARN, "cert[11]: 0x%02x\n", cert[11]));
+  DEBUG((DEBUG_WARN, "cert[12]: 0x%02x\n", cert[12]));
+  DEBUG((DEBUG_WARN, "cert[13]: 0x%02x\n", cert[13]));
+  DEBUG((DEBUG_WARN, "cert[14]: 0x%02x\n", cert[14]));
+*/
 
-  DEBUG((DEBUG_ERROR, "%a exit\n", __FUNCTION__));
+  Status = gRT->SetVariable (
+                  (CHAR16 *)mDeviceCertVariableName,
+                  &gEfiCallerIdGuid,
+                  EFI_VARIABLE_NON_VOLATILE | EFI_VARIABLE_BOOTSERVICE_ACCESS | EFI_VARIABLE_RUNTIME_ACCESS,
+                  certlen,
+                  (VOID *)certptr
+                  );
+
+  if (EFI_ERROR (Status)) {
+    DEBUG ((
+      DEBUG_ERROR,
+      "Provisioning: Failed to save %s variable to non-volatile storage, Status = %r\n",
+      mDeviceCertVariableName,
+      Status
+      ));
+  }
+
+  if(certptr != NULL)
+    FreePool(certptr);
+
+  return Status;
+}
+
+EFI_STATUS
+EFIAPI
+RecieveSmbiosValues()
+{
+  EFI_STATUS Status;
+  UINT8* smbiosptr;
+  UINT32 smbioslen;
+  UINT32 sum;
+  UINT32 i;
+
+  Status = EFI_SUCCESS;
+
+  RecieveBuffer("MFG:smbiosreq\n", &smbiosptr, &smbioslen);
+
+  for(i = 0, sum = 0; i < smbioslen; i++) {
+    sum += smbiosptr[i];
+  }
+
+  DEBUG((DEBUG_ERROR, "Smbios device side checksum: %d\n", sum));
+
+  return Status;
+}
+
+EFI_STATUS
+EFIAPI
+ProvisioningInitialize (
+  IN EFI_HANDLE       ImageHandle,
+  IN EFI_SYSTEM_TABLE *SystemTable
+  )
+{
+  EFI_STATUS Status;
+
+  Status = TransmitEKCertificate();
+  if (EFI_ERROR(Status)) {
+    // Tell the host device that EK certificate retrieval and transmit failed.
+    DEBUG((DEBUG_ERROR, "MFG:ekcertfailure\n", Status));
+    DEBUG((DEBUG_ERROR, "TransmitEKCertificate returned 0x%x\n", Status));
+    return Status;
+  }
+
+  Status = RecieveCrossSignedCert();
+  if (EFI_ERROR(Status)) {
+    // Tell the host device that cross signed cert retrieval failed.
+    DEBUG((DEBUG_ERROR, "MFG:devicecertfailure\n", Status));
+    DEBUG((DEBUG_ERROR, "RecieveCrossSignedCert returned 0x%x\n", Status));
+    return Status;
+  }
+
+  Status = RecieveSmbiosValues();
+  if (EFI_ERROR(Status)) {
+    // Tell the host device that cross signed cert retrieval failed.
+    DEBUG((DEBUG_ERROR, "MFG:smbiosfailure\n", Status));
+    DEBUG((DEBUG_ERROR, "RecieveSmbiosValues returned 0x%x\n", Status));
+    return Status;
+  }
+
+  DEBUG((DEBUG_WARN, "%a exit\n", __FUNCTION__));
   return Status;
 }
